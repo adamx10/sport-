d7ef24590f554e3ceb1721bedc3bb115a3adfc66
adam <hazimaa1254@gmail.com>
2026-06-01 15:49:59 +0100

    first

---
+++ components/home.jsx
@@ -0,0 +1,15 @@
+import {Text , View} from 'react-native'
+
+import React from 'react'
+
+function Home() {
+  return (
+ <View>
+    <Text>
+        hello
+    </Text>
+ </View>
+  )
+}
+
+export default Home
