54989e94d36c7aa20c3f2a880ec9cbe79ada5fef
kadnane371-eng <kadnane371@gmail.com>
2026-06-04 11:49:30 +0200

    Refactor Home component and add API service for fetching sports data

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,13 @@
-import { Text, View } from "react-native";
+import { View,Text } from "react-native";
 
-export default function Index() {
+export default function Home() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ padding: 20 }}>
+      <Text style={{fontSize: 30,fontWeight: "bold",}}>Explorer</Text>
+
+      <Text style={{ color: "gray", marginTop: 10 }}>
+        Découvrez votre prochaine passion
+      </Text>
     </View>
   );
 }
